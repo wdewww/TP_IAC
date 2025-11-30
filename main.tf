d1ef5349d7d458da6c0287604651a2cebb781ac7
youssef <youssef.rouissi41@gmail.com>
2025-11-30 20:02:19 +0100

    adjusted terraform backend

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 terraform {
+
+    backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "wdewww/TP_IAC"
+  }
+    
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
